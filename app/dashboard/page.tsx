e1ab4433ee74304517a810cbffe4719a9a1f5270
israel-zambrana-otero_aonisrael.zambrana.otero@aon.es </Users/a0823688/.ssh/id_rsa_>
2024-03-16 14:08:21 +0100

    complete course

--- app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Page = () => {
-  return <p>Dashboard Page</p>;
-};
-
-export default Page;
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
+// user@nextmail.com
+// 123456
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
